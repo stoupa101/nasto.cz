9b8f064e3eb12c62f81257816bc5d0203eb00732
stoupa101 <sd@nix.cz>
2018-03-01 22:00:32 +0100

    add twig

--- src/Controller/NatyController.php
+++ src/Controller/NatyController.php
@@ -10,16 +10,17 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class NatyController
+class NatyController extends AbstractController
 {
     /**
      * @return Response
      * @Route("/")
      */
     public function homepage() {
-        return new Response('start');
+        return $this->render('naty/homepage.html.twig');
     }
 
 }
